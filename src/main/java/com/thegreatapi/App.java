bb7c47c63d84d104c8138d80de3c2e307dd25b0e
hbelmiro <helber.belmiro@gmail.com>
2022-05-18 20:50:05 -0300

    Added real queries
    Signed-off-by: hbelmiro <helber.belmiro@gmail.com>

--- src/main/java/com/thegreatapi/App.java
+++ src/main/java/com/thegreatapi/App.java
@@ -7,7 +7,11 @@ import java.util.Properties;
 
 public final class App {
 
+    private static final List<String> ESSENTIALS = List.of("human-microbiota-dysbiosis");
+
     public static void main(String[] args) throws IOException {
+
+
         var properties = new Properties();
         properties.load(App.class.getResourceAsStream("/query.properties"));
 
@@ -21,15 +25,32 @@ public final class App {
                                                                    .filter(entry -> entry.getValue().size() > 1)
                                                                    .count());
 
+        System.out.println("Quantidade de artigos: " + parseResult.size() + System.lineSeparator());
+
+        System.out.println("Quantidade de artigos com as queries essenciais e mais uma: "
+                + getTotalThatHaveEssentials(parseResult) + System.lineSeparator());
+
         parseResult.entrySet().stream()
                    .filter(entry -> entry.getValue().size() > 1)
+                   .filter(entry -> mustHave(ESSENTIALS, entry))
                    .sorted((e1, e2) -> e2.getValue().size() - e1.getValue().size())
                    .forEach(e -> System.out.println(e.getKey().getPmid() + " (" + e.getValue().size() + ") " + e.getValue()));
 
-        System.out.println(System.lineSeparator() + "Todos os artigos: " + System.lineSeparator());
+//        System.out.println(System.lineSeparator() + "Todos os artigos: " + System.lineSeparator());
+//
+//        parseResult.entrySet().stream()
+//                   .sorted((e1, e2) -> e2.getValue().size() - e1.getValue().size())
+//                   .forEach(e -> System.out.println(e.getKey().getPmid() + " (" + e.getValue().size() + ") " + e.getValue()));
+    }
 
-        parseResult.entrySet().stream()
-                   .sorted((e1, e2) -> e2.getValue().size() - e1.getValue().size())
-                   .forEach(e -> System.out.println(e.getKey().getPmid() + " (" + e.getValue().size() + ") " + e.getValue()));
+    private static boolean mustHave(List<String> essentialQueries, Map.Entry<Article, List<String>> entry) {
+        return essentialQueries.stream().anyMatch(entry.getValue()::contains);
+    }
+
+    private static long getTotalThatHaveEssentials(Map<Article, List<String>> parseResult) {
+        return parseResult.entrySet().stream()
+                          .filter(entry -> entry.getValue().size() > 1)
+                          .filter(entry -> mustHave(ESSENTIALS, entry))
+                          .count();
     }
 }
